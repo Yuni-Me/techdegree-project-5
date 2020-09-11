bf667fdde6a5b118148383ed133512b193f7108e
Yuni Mata <7897746+Yuni-Me@users.noreply.github.com>
2020-09-11 03:49:25 -0700

    Add the filter search feature

---
+++ js/searchScript.js
@@ -0,0 +1,16 @@
+function filter() {
+    const searchValue = document.querySelector('#searchInput').value;
+    const article = document.querySelectorAll('.gallery a');
+
+    for (let i = 0; i < article.length; i++) {
+        if (article[i].getAttribute('data-caption').toLowerCase().includes(searchValue.toLowerCase())) {
+            article[i].style.display = 'block';
+            
+        } else {
+            article[i].style.display = 'none';
+            
+        }
+        
+    }
+    
+}
